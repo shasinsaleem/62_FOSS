ca87cc4dbf64d29f94ba76668efb46efb0f2624a
Shasin <s4csb@ccfpc63-OptiPlex-390.(none)>
2017-03-28 15:50:31 +0530

    hash

---
+++ hash.pl
@@ -0,0 +1,8 @@
+print "ENTER THE VALUE OF JOHN\n";
+$hash{john}=<STDIN>;
+print "ENTER THE VALUE OF JIBIN\n";
+$hash{jibin}=<STDIN>;
+print "keys are";
+print @array=keys %hash;
+print "values are";
+print @array=values %hash;
